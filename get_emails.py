0efc99781c69005612134bf4755f7c97c498792b
Tong Li <tonli@redhat.com>
2016-08-22 10:50:06 +0800

    add linux-initramfs mailing list support

--- get_emails.py
+++ get_emails.py
@@ -59,6 +59,71 @@ class LKML(GeneralList):
                         break
             week_id += 1
 
+class Spinics(GeneralList):
+    """ Class for retrieving emails from mailing lists on verg.kernel.org """
+    url_base = 'http://www.spinics.net/lists/'
+
+    def _retrieve(self, options, list_name=None):
+        self.over = False
+        # Email pattern
+        patterns = []
+        for email in options.email:
+            email_user, email_domain = re.split('@', email)
+            patterns.append('{0} &lt;{1}@{2}&gt;'.format(options.name, email_user, 'x' * len(email_domain)))
+        # The range of the dates we want to search
+        date_range = [day for day in calendar.Calendar().itermonthdates(options.year, options.month)
+                     if day.month == options.month]
+        # Search for match in the first page
+        first_url = '{0}{1}/maillist.html'.format(self.url_base,
+                                                     list_name,)
+        try:
+            page = urllib.request.urlopen(first_url).read()
+            self._search_in_page(page, list_name, patterns, date_range)
+        except urllib.error.HTTPError:
+            print('URL {0} does not exist'.format(first_url))
+            self.emails = None
+            return
+
+        # Search in the rest ones
+        url_id = 2
+        while True:
+            iterate_url = '{0}{1}/mail{2}.html'.format(self.url_base,
+                                                       list_name,
+                                                       url_id)
+            try:
+                page = urllib.request.urlopen(iterate_url).read()
+                if not self.over:
+                    self._search_in_page(page, list_name, patterns, date_range)
+            except urllib.error.HTTPError:
+                break
+            url_id += 1
+
+    def _search_in_page(self, page, list_name, patterns, date_range):
+        lines = page.split(b'<li><strong>')
+        # Magic number stores infomation we need
+        for line in lines[1:]:
+            item = re.split('[<>]', line.decode('utf-8'))
+            if any(match in item[14] for match in patterns):
+                subject = item[2]
+                detail_url = '{0}{1}/{2}'.format(self.url_base,
+                                               list_name,
+                                               item[1].split()[2].split('=')[1].replace('"', ''))
+
+                detail_lines = urllib.request.urlopen(detail_url).read().split(b'\n')
+                message_id, date = None, None
+                for line in detail_lines:
+                    if b'X-Date:' in line:
+                        date = dateparser.parse(line.decode('utf-8')[len('<!--X-Date: '):-len(' -->')].replace('&#45;', '-')).date()
+                        if date < date_range[0]:
+                            self.over = True
+                    elif b'X-Message-Id:' in line:
+                        message_id = line.decode('utf-8')[len('<!--X-Message-Id: '):-len(' -->')].replace('&#45;', '-')
+                    elif message_id and date and not self.over:
+                        self.emails[message_id] = (subject, str(date))
+                        return
+                    elif self.over:
+                        return
+
 class RHInternal(GeneralList):
     """ Class for retrieving emails from internal Red Hat lists """
     url_base = 'http://post-office.corp.redhat.com/archives/'
