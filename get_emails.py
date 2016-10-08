d1eef1d521b655248fc0d5b132b880ea457c294d
Tong Li <tonli@redhat.com>
2016-10-08 11:15:22 +0800

    PEP-8

--- get_emails.py
+++ get_emails.py
@@ -14,11 +14,13 @@ class GeneralList(object):
 
     def __init__(self, options, list_name=None):
         self.emails = {}
-        print("Search {0} From {1}".format(list_name, self.url_base), file=sys.stderr)
+        print("Searching {0} From {1}".format(
+            list_name, self.url_base), file=sys.stderr)
         self._retrieve(options, list_name)
 
     def _retrieve(self, options, list_name=None):
         raise NotImplementedError
+
 
 class LKML(GeneralList):
     """ Class for retrieving from another archiver of LKML """
@@ -26,16 +28,21 @@ class LKML(GeneralList):
 
     def _retrieve(self, options, list_name=None):
 
+        # Pattern is like Name &lt;foo@xxxxxxx&gt;
         patterns = []
         for email in options.email:
             email_user, email_domain = re.split('@', email)
-            patterns.append('{0} &lt;{1}@{2}&gt;'.format(options.name, email_user, 'x' * len(email_domain)))
+            patterns.append('{0} &lt;{1}@{2}&gt;'.format(
+                options.name, email_user, 'x' * len(email_domain)))
+        # This archiver stores emails in week-based period
         week_id = 0
+        first_day = datetime.date(options.year, options.month, 1)
         while True:
-            url = '{0}{1}{2}.{3}/author.html'.format(self.url_base,
-                                                     options.year % 100,
-                                                     str(options.month).zfill(2),
-                                                     week_id)
+            url = '{0}{1}{2}.{3}/author.html'.format(
+                    self.url_base,
+                    options.year % 100,
+                    str(options.month).zfill(2),
+                    week_id)
             try:
                 lines = urllib.request.urlopen(url).readlines()
             except urllib.error.HTTPError:
@@ -51,16 +58,24 @@ class LKML(GeneralList):
             for thread in threads:
                 item = re.split('[<>]', thread.decode('utf-8'))
                 subject, date = item[8], dateparser.parse(item[14]).date()
-                detail_url = '{0}{1}'.format(url[:-len('author.html')],
-                                              item[7].split()[-1].split("=")[-1].strip('"'))
-                detail_lines = urllib.request.urlopen(detail_url).read().decode('utf-8').split("\n")
+                if date < first_day:
+                    continue
+                detail_url = '{0}{1}'.format(
+                        url[:-len('author.html')],
+                        item[7].split()[-1].split("=")[-1].strip('"'))
+                detail_lines = urllib.request.urlopen(detail_url).\
+                    read().decode('utf-8').split("\n")
                 for line in detail_lines:
                     if 'X-Message-Id:' in line:
-                        message_id = line[len('<!--X-Message-Id: '):-len(' -->')].replace('&#45;', '-')
+                        m_id_start = len('<!--X-Message-Id: ')
+                        m_id_end = -len(' -->')  # Reversed index
+                        m_id_info = line[m_id_start:m_id_end]
+                        message_id = m_id_info.replace('&#45;', '-')
                     if any(match in line for match in patterns):
                         self.emails[message_id] = (subject, str(date))
                         break
             week_id += 1
+
 
 class Spinics(GeneralList):
     """ Class for retrieving emails from www.spinics.com """
@@ -68,17 +83,20 @@ class Spinics(GeneralList):
 
     def _retrieve(self, options, list_name=None):
         self.over = False
-        # Email pattern
+        # Pattern is the same as LKML's one
         patterns = []
         for email in options.email:
             email_user, email_domain = re.split('@', email)
-            patterns.append('{0} &lt;{1}@{2}&gt;'.format(options.name, email_user, 'x' * len(email_domain)))
+            patterns.append('{0} &lt;{1}@{2}&gt;'.format(
+                options.name, email_user, 'x' * len(email_domain)))
         # The range of the dates we want to search
-        date_range = [day for day in calendar.Calendar().itermonthdates(options.year, options.month)
-                     if day.month == options.month]
+        date_range = [
+                day for day in
+                calendar.Calendar().itermonthdates(options.year, options.month)
+                if day.month == options.month]
         # Search for match in the first page
         first_url = '{0}{1}/maillist.html'.format(self.url_base,
-                                                     list_name,)
+                                                  list_name,)
         try:
             page = urllib.request.urlopen(first_url).read()
             self._search_in_page(page, list_name, patterns, date_range)
@@ -90,11 +108,11 @@ class Spinics(GeneralList):
         # Search in the rest ones
         url_id = 2
         while True:
-            iterate_url = '{0}{1}/mail{2}.html'.format(self.url_base,
+            current_url = '{0}{1}/mail{2}.html'.format(self.url_base,
                                                        list_name,
                                                        url_id)
             try:
-                page = urllib.request.urlopen(iterate_url).read()
+                page = urllib.request.urlopen(current_url).read()
                 if not self.over:
                     self._search_in_page(page, list_name, patterns, date_range)
             except urllib.error.HTTPError:
@@ -108,33 +126,48 @@ class Spinics(GeneralList):
             item = re.split('[<>]', line.decode('utf-8'))
             if any(match in item[14] for match in patterns):
                 subject = item[2]
+                herf = item[1].split()[2].split('=')[1].replace('"', '')
                 detail_url = '{0}{1}/{2}'.format(self.url_base,
-                                               list_name,
-                                               item[1].split()[2].split('=')[1].replace('"', ''))
+                                                 list_name,
+                                                 herf)
 
-                detail_lines = urllib.request.urlopen(detail_url).read().split(b'\n')
+                detail_page = urllib.request.urlopen(detail_url)
+                detail_lines = detail_page.read().split(b'\n')
                 message_id, date = None, None
                 for line in detail_lines:
                     if b'X-Date:' in line:
-                        date = dateparser.parse(line.decode('utf-8')[len('<!--X-Date: '):-len(' -->')].replace('&#45;', '-')).date()
+                        d_start = len('<!--X-Date: ')
+                        d_end = -len(' -->')
+                        d_info = line.decode('utf-8')[d_start:d_end]
+                        date = dateparser.parse(d_info.replace('&#45;', '-'))
+                        if date is not None:
+                            date = date.date()
                         if date < date_range[0]:
                             self.over = True
                     elif b'X-Message-Id:' in line:
-                        message_id = line.decode('utf-8')[len('<!--X-Message-Id: '):-len(' -->')].replace('&#45;', '-')
+                        m_id_start = len('<!--X-Message-Id: ')
+                        m_id_end = -len(' -->')
+                        message_id = line.decode('utf-8')[m_id_start:m_id_end]
+                        # This HTML pages use &#45 instead of -
+                        message_id = message_id.replace('&#45;', '-')
                     elif message_id and date and not self.over:
                         self.emails[message_id] = (subject, str(date))
                         return
                     elif self.over:
                         return
 
+
 class GzipArchived(GeneralList):
-    """ Base class for lists which provide downloadable gziped archive files """
+    """
+    Base class for lists which provide downloadable gziped archvie files
+    """
     def __init__(self, options, url=None, list_name=None):
         if url is not None:
             self.url_base = url
         super().__init__(options, list_name)
 
     def _parse_gz_archive(self, url, options):
+        """ Method used to parse information from gziped archive """
         try:
             with urllib.request.urlopen(url) as gz_archive:
                 with gzip.open(gz_archive, 'r') as gz_file:
@@ -144,10 +177,11 @@ class GzipArchived(GeneralList):
             self.emails = None
             return
         # Some archiver stores email address as "foo at bar.com" format
-        patterns  = [b'From ' + email.encode('utf-8')
-                     for email in options.email] + \
-                    [b'From ' + email.replace('@', ' at ').encode('utf-8')
-                     for email in options.email]
+        # Examples kexec upstream and kexec-fedora
+        patterns = [b'From ' + email.encode('utf-8')
+                    for email in options.email] + \
+                   [b'From ' + email.replace('@', ' at ').encode('utf-8')
+                    for email in options.email]
         for index, line in enumerate(lines):
             if any(match in line for match in patterns):
                 # Sometimes a subject may be splited into multiple lines
@@ -157,40 +191,66 @@ class GzipArchived(GeneralList):
                        subject_end is None:
                         subject_start = index2
                     elif re.match(b'^.*:\s.*', line2) and \
-                         subject_start is not None:
+                            subject_start is not None:
                         subject_end = index2
-                        subject = b' '.join(item.strip() for item in lines[index + 1:][subject_start:subject_end]).\
-                                                                                  decode('utf-8')[len('Subject: '):]
+                        subject = b' '.join(
+                                item.strip() for item in
+                                lines[index + 1:][subject_start:subject_end])
+                        subject = subject.decode('utf-8')[len('Subject: '):]
                         subject_start = None
                     elif re.match(b'^From\s.*', line2) or \
-                         index2 == len(lines[index + 1:]) - 1:
+                            index2 == len(lines[index + 1:]) - 1:
                         in_reply_to = False
                         for next_line in lines[index + 1:index + 1 + index2]:
-                            if re.match(b'^In-Reply-To: .*', next_line, re.IGNORECASE):
+                            if re.match(b'^In-Reply-To: .*',
+                                        next_line,
+                                        re.IGNORECASE):
                                 in_reply_to = True
-                            if re.match(b'^Message-ID: .*', next_line, re.IGNORECASE):
-                                message_id = next_line[len(b'Message-ID: '):].decode().strip('<>')
-                            elif re.match(b'^Date: .*', next_line, re.IGNORECASE):
+                            if re.match(b'^Message-ID: .*',
+                                        next_line,
+                                        re.IGNORECASE):
+                                message_id = next_line[len(b'Message-ID: '):]
+                                message_id = message_id.decode().strip('<>')
+                            elif re.match(b'^Date: .*',
+                                          next_line,
+                                          re.IGNORECASE):
                                 date_info = next_line[len(b'Date: '):].decode()
-                        patch_start = [start_index for start_index, start_line in enumerate(lines[index + 1:index + 1 + index2])
-                                                       if re.match(b'^--- .*', start_line)]
+                        # Confirm whether there is a patch included
+                        patch_start = [
+                                start_index for start_index, start_line in
+                                enumerate(lines[index + 1:index + 1 + index2])
+                                if re.match(b'^--- .*', start_line)]
                         patch_included = False
-                        for start_index in patch_start:
-                            if index + 1 + start_index + 2 < len(lines):
-                                patch_included = re.match(b'^\+\+\+ .*', lines[index + 1 + start_index + 1]) is not None and \
-                                                 re.match(b'^@@ .*', lines[index + 1 + start_index + 2]) is not None or \
-                                                 patch_included
+                        for p_start in patch_start:
+                            if index + 1 + p_start + 2 < len(lines):
+                                s_line = re.match(b'^\+\+\+ .*',
+                                                  lines[index + 1 +
+                                                        p_start + 1])
+                                t_line = re.match(b'^@@ .*',
+                                                  lines[index + 1 +
+                                                        p_start + 2])
+                                patch_included = \
+                                    s_line is not None and \
+                                    t_line is not None or \
+                                    patch_included
+                        # Some archives don't include 'Re:' for replies
+                        re_included = re.match('^re:.*|.*\sre:\s.*',
+                                                   subject,
+                                                   re.IGNORECASE)
                         if in_reply_to and \
-                           not re.match('^re:.*|.*\sre:\s.*', subject, re.IGNORECASE) and \
+                           not re_included and \
                            not patch_included:
                             subject = 'Re: ' + subject
                         break
+                # dateparser can't parse date like '2016, Aug, 28, -0500 (EST)'
                 if date_info.find('(') != -1:
                     date_info = date_info[:date_info.find('(') - 1]
                 date = dateparser.parse(date_info)
+                # In case we got a date_info which dateparser can't parse
                 if date:
                     date = date.date()
                 self.emails[message_id] = (subject, str(date))
+
 
 class RHInternal(GzipArchived):
     """ Class for retrieving emails from internal Red Hat lists(deprecated) """
@@ -205,21 +265,27 @@ class RHInternal(GzipArchived):
                                              month)
         super()._parse_gz_archive(url, options)
 
+
 class Pipermail(GzipArchived):
-    """ Class for retrieving emails from pipermail, default archiver of mailman 2 """
+    """
+    Class for retrieving emails from pipermail, default archiver of mailman 2
+    """
     url_base = 'http://lists.infradead.org/pipermail/'
 
     def _retrieve(self, options, list_name=None):
         # Pipermail uses the same format as RHInternal archiver
         month = datetime.date(options.year, options.month, 1).strftime('%B')
         url = '{0}{1}/{2}-{3}.txt.gz'.format(self.url_base,
-                                           list_name,
-                                           options.year,
-                                           month)
+                                             list_name,
+                                             options.year,
+                                             month)
         super()._parse_gz_archive(url, options)
 
+
 class HyperKitty(GzipArchived):
-    """ Class for retrieving emails from HyperKitty, default archiver of mailman 3 """
+    """
+    Class for retrieving emails from HyperKitty, default archiver of mailman 3
+    """
     url_base = 'https://lists.fedoraproject.org/archives/'
 
     def _retrieve(self, options, list_name=None):
@@ -227,11 +293,11 @@ class HyperKitty(GzipArchived):
         domain = self.url_base.split('/')[2]
         month = str(options.month).zfill(2)
         next_month = str(options.month + 1).zfill(2)
-        # Example: https://lists.fedoraproject.org/archives/list/kexec@lists.fedoraproject.org/export/kexec@lists.fedoraproject.org-2016-01.mbox.gz?start=2016-01-01&end=2016-02-01
-        url = '{0}list/{1}@{2}/export/{1}@{2}-{3}-{4}.mbox.gz?start={3}-{4}-01&end={3}-{5}-01'.format(self.url_base,
-                                                                                                      list_name,
-                                                                                                      domain,
-                                                                                                      options.year,
-                                                                                                      month,
-                                                                                                      next_month)
+        url = ("{0}list/{1}@{2}/export/{1}@{2}-{3}-{4}.mbox.gz?"
+               "start={3}-{4}-01&end={3}-{5}-01").format(self.url_base,
+                                                         list_name,
+                                                         domain,
+                                                         options.year,
+                                                         month,
+                                                         next_month)
         super()._parse_gz_archive(url, options)
--- main.py
+++ main.py
@@ -9,11 +9,12 @@ import sys
 
 
 def print_email(count, date, subject):
-    # Differentiate between nonexistent mailing list and no found emails
     if count == 1:
-        print ('{0:>8} {1} {2}'.format(' ', date, subject))
+        print('{0:>8} {1} {2}'.format(' ', date, subject))
     else:
-        print ('{0:>8} {1} {2}'.format(str(count).join(("[", "]")), date, subject))
+        print('{0:>8} {1} {2}'.format(
+            str(count).join(("[", "]")), date, subject))
+
 
 def main():
     """
@@ -24,46 +25,52 @@ def main():
     Gather lists of all sent mails for given user during specified time and
     print them.
     """
-    #options = config_options.Options(sys.argv[1:])
-    # Email is set only if all needed options are specified
-    #if options.email is None or \
-    #   options.name is None:
-    #    if len(sys.argv) > 1:
-    #        print('Falling back to configuration file')
-        # Needed options in command are missing, use config
+
+    # Parse options from both configuration file and command line
     options = config_options.Config(sys.argv[1:])
     if options.email is None or \
        options.name is None or \
        options.year is None or \
        options.month is None:
         return
-
-    print('Searching for {0} <{1}>'.format(options.name, str(options.email).strip('[]')), file=sys.stderr)
+    # Start searching
+    searching_email = ' '.join(
+            email.strip("'") for email in options.email)
+    print('Searching for {0} <{1}>'.format(
+        options.name, searching_email), file=sys.stderr)
     # Ths dict's structure is {message-id: (subject, date)}
     emails = {}
     if options.lkml:
         emails.update(get_emails.LKML(options, "lkml").emails)
     for url, mailing_lists in options.pipermail.items():
         for mailing_list in mailing_lists:
-            emails.update(get_emails.Pipermail(options, url, mailing_list).emails)
+            emails.update(get_emails.Pipermail(
+                options, url, mailing_list).emails)
     for url, mailing_lists in options.hyperkitty.items():
         for mailing_list in mailing_lists:
-            emails.update(get_emails.HyperKitty(options, url, mailing_list).emails)
+            emails.update(get_emails.HyperKitty(
+                options, url, mailing_list).emails)
     for mailing_list in options.spinics:
-        emails.update(get_emails.Spinics(options, mailing_list).emails)
-
+        emails.update(get_emails.Spinics(
+            options, mailing_list).emails)
     emails = [info for message_id, info in emails.items()]
-    patched, replied  = [], []
-    patched_count, replied_count = 0, 0
+    patched, replied, others = [], [], []
+    patched_count, replied_count, others_count = 0, 0, 0
+    # Count emails which have the same subject
     filtered_emails = {}
-    print('Messages:')
     for subject, date in emails:
         if subject in filtered_emails:
+            if date > filtered_emails[subject][0]:
+                filtered_emails[subject][0] = date
             filtered_emails[subject][1] += 1
         else:
             filtered_emails[subject] = [date, 1]
-    filtered_emails = [(count, date, subject) for subject, (date, count) in filtered_emails.items()]
+    filtered_emails = [
+            (count, date, subject) for subject, (date, count) in
+            filtered_emails.items()]
     filtered_emails.sort(key=lambda tup: tup[1])
+    # Output result
+    print('Messages:')
     for count, date, subject in filtered_emails:
         print_email(count, date, subject)
         if re.match('^re:.*|.*\sre:\s.*', subject, re.IGNORECASE):
@@ -72,13 +79,21 @@ def main():
         elif re.match('.*\Wpatch\W.*', subject, re.IGNORECASE):
             patched.append((count, date, subject))
             patched_count += count
-    print('Patches:')
+        else:
+            others.append((count, date, subject))
+            others_count += count
+    print('Patched:')
     for count, date, subject in patched:
         print_email(count, date, subject)
     print('Replied:')
     for count, date, subject in replied:
         print_email(count, date, subject)
-    print('{0} message(s) found, {1} patched, {2} replied'.format(len(emails), patched_count, replied_count))
+    print('Others:')
+    for count, date, subject in others:
+        print_email(count, date, subject)
+    print("{0} message(s) found, {1} patched, {2} replied, {3} others".format(
+                        len(emails), patched_count,
+                        replied_count, others_count))
 
 if __name__ == '__main__':
     main()
